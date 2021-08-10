80302bd0ee382e57c75056854907d37fc0b7b2f2
Bass, David <d-bass@outlook.com>
2021-08-10 12:35:40 -0400

    First commit

---
+++ hello.php
@@ -0,0 +1,12 @@
+<!-- Opening Tag in PHP, this tells the Web Server some PHP code is going to follow bellow -->
+<?php
+
+//  'message' variable
+
+$message = "Hello again";
+
+//  echo - means whatever follows 'echo' should be printed out to the browser, Quotes are used, so Echo knows where the message starts an ends.  The ; marks the end of this instruction
+
+// echoing the  variable $message contents
+echo $message;
+
